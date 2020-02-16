153b7481128a6a9fa37ee75366f807da8c9de0d5
Karol Pręga <karol.prega@gmail.com>
2020-02-16 18:44:26 +0100

    Added GeometryExtensions class

---
+++ Extensions/GeometryExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Geometry3d;
+
+namespace Tekla.Structures.OpenApi
+{
+    public static class GeometryExtensions
+    {
+        /// <summary>
+        /// Adds two vectors.
+        /// </summary>
+        /// <param name="vector">Vector to add to.</param>
+        /// <param name="vectorToBeAdded">Vector to be added.</param>
+        /// <returns>Sum of given vectors.</returns>
+        public static Vector Add(this Vector vector, Vector vectorToBeAdded)
+        {
+            vector.X += vectorToBeAdded.X;
+            vector.Y += vectorToBeAdded.Y;
+            vector.Z += vectorToBeAdded.Z;
+            return vector;
+        }
+
+        /// <summary>
+        /// Subtracts two vectors.
+        /// </summary>
+        /// <param name="vector">Vector to subtract from.</param>
+        /// <param name="vectorToBeSubtracted">Vector to be subtracted.</param>
+        /// <returns>Difference of given vectors.</returns>
+        public static Vector Subtract(this Vector vector, Vector vectorToBeSubtracted)
+        {
+            return vector.Add(-1 * vectorToBeSubtracted);
+        }
+
+        /// <summary>
+        /// Translates point by given vector.
+        /// </summary>
+        /// <param name="point">Point to be translated.</param>
+        /// <param name="vector">Translation vector.</param>
+        public static void Translate(this Point point, Vector vector)
+        {
+            point.Translate(vector.X, vector.Y, vector.Z);
+        }
+    }
+
+
+}
