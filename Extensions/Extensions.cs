7264781d1033b90f996ec2767338f691e0a60c9d
Karol Pręga <karol.prega@gmail.com>
2020-02-16 12:01:50 +0100

    First commit

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekla.Structures.OpenApi
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Creates list of objects with given type from object implementing IEnumerator interface.
+        /// </summary>
+        /// <typeparam name="T">Type of objects in the list.</typeparam>
+        /// <param name="enumerator">Object implementing IEnumerator interface.</param>
+        /// <returns>List of objects of given type.</returns>
+        public static List<T> ToList<T>(this IEnumerator enumerator)
+        {
+            var list = new List<T>();
+            while (enumerator.MoveNext())
+            {
+                if (enumerator.Current is T) list.Add((T)enumerator.Current);
+            }
+            return list;
+        }
+    }
+}
